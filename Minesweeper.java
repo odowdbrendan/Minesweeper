299021c90ae8096d496cbd6ef5d37d10019179bf
Brendan O'Dowd <31706197+odowdbrendan@users.noreply.github.com>
2020-12-02 15:03:13 -0500

    Update Minesweeper.java

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,10 +1,30 @@
+import java.util.Scanner;
 
 public class Minesweeper {
 
-	public static Cell [][] gameboard = new Cell [10][10];
+	private static boolean playAgain() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Would you like to play again?");
+		String userResponse = scan.nextLine();
+		boolean playAgain = false;
 
-	public static void main (String args[]) {	
-		Gameboard gameBoard = new Gameboard(gameboard);					
-		gameBoard.run();
-	}	
+		if (userResponse.matches("(?i)yes|y")) {
+			playAgain = true;
+		} else if (!userResponse.matches("(?i)no|n")) {
+			System.out.println("Invalid input. Please try again.");
+			playAgain = playAgain();
+		} 
+
+		return playAgain;
+	}
+
+	public static void main(String args[]) {
+		boolean playGame = true;
+
+		while (playGame) {
+			Gameboard gameBoard = new Gameboard();
+			gameBoard.run();
+			playGame = playAgain();
+		}
+	}
 }
